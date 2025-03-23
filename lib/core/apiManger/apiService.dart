7884ea8a42844d316dd1892f37ad4b04efcd06ee
FatmaNasser29 <fatma.nasser1213@gmail.com>
2025-03-23 16:57:09 +0200

    update retrofit configuration

--- lib/core/apiManger/apiService.dart
+++ lib/core/apiManger/apiService.dart
@@ -2,5 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+part 'apiService.g.dart';
+
 @RestApi(baseUrl: "https://jsonplaceholder.typicode.com/")
-abstract class ApiService {}
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+}
