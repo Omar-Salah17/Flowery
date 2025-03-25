567314b8f5f47b966945733123dd4231248ad870
Mariam2572 <mariamkamel791@gmail.com>
2025-03-25 05:49:28 +0200

    Add registration feature with cubit state management and spacing helpers

--- lib/features/auth/register/presentation/view/screens/register.dart
+++ lib/features/auth/register/presentation/view/screens/register.dart
@@ -1,3 +1,4 @@
+import 'package:flowery/core/helper/spacing.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatelessWidget {
@@ -5,6 +6,23 @@ class Register extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var text = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        title:   Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(Icons.arrow_back_ios),
+          horizontalSpace(10),
+            Text('Sign Up', style: text.titleMedium,),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
