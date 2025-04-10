4c8a16725204fa6642bc28da4270e2bfdeaba55f
FatmaNasser29 <fatma.nasser1213@gmail.com>
2025-04-10 05:23:26 +0200

    add cubit to presentation

--- lib/features/productsDetails/presentation/widgets/product_details_body.dart
+++ lib/features/productsDetails/presentation/widgets/product_details_body.dart
@@ -1,5 +1,9 @@
 import 'package:flowery/core/utils/colors.dart';
+import 'package:flowery/features/productsDetails/data/model/product_details_model.dart';
+import 'package:flowery/features/productsDetails/presentation/viewModel/product_details_cubit/product_details_cubit.dart';
+import 'package:flowery/features/productsDetails/presentation/viewModel/product_details_cubit/product_details_states.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class ProductDetailsBody extends StatefulWidget {
@@ -32,54 +36,66 @@ class _ProductDetailsBodyState extends State<ProductDetailsBody> {
         ),
       ),
     );
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 16),
-            SizedBox(
-              height: 240,
-              child: PageView.builder(
-                controller: controller,
-                // itemCount: pages.length,
-                itemBuilder: (_, index) {
-                  return pages[index % pages.length];
-                },
-              ),
-            ),
-
-            Center(
-              child: SmoothPageIndicator(
-                controller: controller,
-                count: pages.length,
-                effect: const WormEffect(
-                  dotHeight: 16,
-                  dotWidth: 16,
-                  type: WormType.thinUnderground,
-                  activeDotColor: PalletsColors.mainColorBase,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16),
+    return BlocBuilder<ProductDetailsCubit, ProductDetailsState>(
+      builder: (context, state) {
+        if (state is ProductDetailsLoading) {
+          return Center(child: CircularProgressIndicator());
+        } else if (state is ProductDetailsError) {
+          return Center(child: Text('Error: ${state.message}'));
+        } else if (state is ProductDetailsSuccess) {
+          List<ProductsModel> product = state.product;
+          return SafeArea(
+            child: SingleChildScrollView(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(children: [Text("data"), Spacer(), Text("data")]),
-                  Text("data"),
-                  Text("data"),
-                  Text("data"),
-                  Text("data"),
-                  Text("data"),
-                  Text("data"),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 16),
+                  SizedBox(
+                    height: 240,
+                    child: PageView.builder(
+                      controller: controller,
+                      // itemCount: pages.length,
+                      itemBuilder: (_, index) {
+                        return pages[index % pages.length];
+                      },
+                    ),
+                  ),
+
+                  Center(
+                    child: SmoothPageIndicator(
+                      controller: controller,
+                      count: pages.length,
+                      effect: const WormEffect(
+                        dotHeight: 16,
+                        dotWidth: 16,
+                        type: WormType.thinUnderground,
+                        activeDotColor: PalletsColors.mainColorBase,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(children: [Text("data"), Spacer(), Text("data")]),
+                        Text("data"),
+                        Text("data"),
+                        Text("data"),
+                        Text("data"),
+                        Text("data"),
+                        Text("data"),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
+          );
+        }
+        return Container();
+      },
     );
   }
 }
