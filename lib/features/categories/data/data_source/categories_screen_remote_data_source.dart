e973afb3d2b03c47c426d2cfce3e8009a48bed28
Samah <Samahosama112@icloud.com>
2025-04-12 17:01:26 +0200

    refactor for development code

--- lib/features/categories/data/data_source/categories_screen_remote_data_source.dart
+++ lib/features/categories/data/data_source/categories_screen_remote_data_source.dart
@@ -1,5 +1,5 @@
 import 'package:flowery/features/categories/data/models/categories_model/categories_model.dart';
-import 'package:flowery/features/categories/data/models/products_model/products_model.dart';
+import 'package:flowery/core/utils/models/products_model/products_model.dart';
 
 abstract class CategoriesScreenRemoteDataSource {
   Future<CategoriesModel> getCategories();
