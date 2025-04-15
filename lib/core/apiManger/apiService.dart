7b9763f7f83a06d7df3dccb13cf9830a2cc817a3
Samah <Samahosama112@icloud.com>
2025-04-16 01:03:46 +0200

    Refactor imports in apiService.dart: remove unused imports and clean up code structure

--- lib/core/apiManger/apiService.dart
+++ lib/core/apiManger/apiService.dart
@@ -1,24 +1,22 @@
 import 'dart:io';
-import 'package:http_parser/http_parser.dart';
+
 import 'package:dio/dio.dart';
 import 'package:flowery/core/utils/constants.dart';
-import 'package:flowery/core/utils/models/products_model/product.dart';
 import 'package:flowery/core/utils/models/products_model/products_model.dart';
 import 'package:flowery/features/auth/login/data/models/login_request.dart'
     show LoginRequest;
 import 'package:flowery/features/auth/login/data/models/login_respose.dart';
-import 'package:flowery/features/auth/login/presentation/view/screens/login.dart';
 import 'package:flowery/features/auth/register/data/models/register_request.dart';
 import 'package:flowery/features/auth/register/data/models/register_response.dart';
 import 'package:flowery/features/best_seller/data/models/best_seller_model.dart';
 import 'package:flowery/features/categories/data/models/categories_model/categories_model.dart';
+import 'package:flowery/features/productsDetails/data/models/product_details_model/product_details_model.dart';
 import 'package:flowery/features/profile/data/model/user_response.dart';
-import 'package:flowery/features/productsDetails/data/models/product_details_model/product_details_model.dart';import 'package:flowery/features/profile/data/models/profile_response.dart';
-
-
+import 'package:flowery/features/profile/data/models/profile_response.dart';
 import 'package:retrofit/retrofit.dart';
 
 import '../../features/occasion/data/models/occaions.dart';
+
 part 'apiService.g.dart';
 
 @RestApi(baseUrl: Constants.baseUrl)
