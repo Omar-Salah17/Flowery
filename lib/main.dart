3745ea9d0449830c7f40d224c98d19045d7ffa2e
Samah <Samahosama112@icloud.com>
2025-04-16 21:23:48 +0200

    Fix initial route in main.dart: revert to initial route for application startup

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class Flowery extends StatelessWidget {
       builder: (_, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          initialRoute: RoutesName.editProfile,
+          initialRoute: RoutesName.initial,
           onGenerateRoute: RouteGenerator.onGenerator,
           theme: ApplicationTheme.themeData,
         );
